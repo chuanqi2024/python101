3dc2f544b3e3cdc76f03d6d39f743856eab1b3a1
ChuanqiMa <chuanqima989@gmail.com>
2025-05-24 22:29:00 +0800

    feat:add plotting module

---
+++ src/python101/pycounts.py
@@ -0,0 +1,24 @@
+
+from string import punctuation
+from typing import Counter
+
+
+def load_text(input_file):
+    with open(input_file, "r") as file:
+        text = file.read()
+    return text
+
+
+def clean_text(text):
+    text = text.lower()
+    for p in punctuation:
+        text = text.repalce(p, "")
+    return text
+
+
+def count_words(input_file):
+    text = load_text(input_file)
+    text = clean_text(text)
+    words = text.split()
+    return Counter(words)
+
